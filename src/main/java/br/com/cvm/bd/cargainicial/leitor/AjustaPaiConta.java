563b47866357c0c2b8e8293e63d68e7fd0d1c434
renatotn7 <rnati@TIBRSP-W0004.sistran.local>
2019-12-06 17:30:17 -0300

    asdfsdf

--- src/main/java/br/com/cvm/bd/cargainicial/leitor/AjustaPaiConta.java
+++ src/main/java/br/com/cvm/bd/cargainicial/leitor/AjustaPaiConta.java
@@ -23,9 +23,35 @@ public static  void close(){
 	public static void main(String[] args) {
 		
 
-	   	 Query queryanalisar = em.createQuery("Select c from ValorContabil c where c.contaContabil. = (SELECT min(e.demonstrativo.idDemonstrativo) FROM ContaContabil e where e.analise=0) and  c.analise=0");
+	   	 Query queryanalisar = em.createQuery("Select c from ContaContabil c where c.contaPai is null");
 	
 		 List<ContaContabil>  contaAnalisar = (List<ContaContabil>) queryanalisar.getResultList();
+		 
+		 
+		 for(ContaContabil cc :contaAnalisar ) {
+			 try {
+			String[] niveisConta1 = cc.getContaContabil().split("\\.");
+				String contaInicial1 = niveisConta1[0];
+				String contasPai1[] = new String[niveisConta1.length-1];
+				String sinicial1 = cc.getTipoDemonstrativo().getSiglaTipo();
+				String scPai = contaInicial1;
+				for(int i=1;i<niveisConta1.length-1;i++) {
+					scPai+="."+niveisConta1[i];
+				}
+				 Query querypai = em.createQuery("Select c.contaContabil from ValorContabil c where c.contaContabil.contaContabil ='"+scPai+"' and c.demonstrativo.idDemonstrativo="+cc.getDemonstrativo().getIdDemonstrativo() );
+					
+				 ContaContabil  contaPai = (ContaContabil) querypai.getSingleResult();
+				 cc.setContaPai(contaPai);
+				 em.getTransaction()
+			        .begin();
+					persiste(cc);
+					em.getTransaction()
+			        .commit();
+			 }catch(Exception e){
+				 e.printStackTrace();
+			 }
+				
+		 }
 		// TODO Stub de método gerado automaticamente
 		//String[] niveisConta1 = cc.getContaContabil().split("\\.");
 		//String contaInicial1 = niveisConta1[0];

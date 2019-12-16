e0d823fb4f0652542dec7e275dcf6dd8d42d30e4
renatotn7@gmail.com <renatotn7@gmail.com>
2019-12-16 13:45:43 -0300

    commit

--- src/main/java/br/com/cvm/rest/Application.java
+++ src/main/java/br/com/cvm/rest/Application.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package br.com.cvm.rest;
 
 import java.util.Arrays;
@@ -36,42 +36,3 @@ public class Application {
 	  }*/
 
 }
-=======
-package br.com.cvm.rest;
-
-import java.util.Arrays;
-import java.util.Collections;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-@SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
-		// TODO Stub de método gerado automaticamente
-		 SpringApplication.run(Application.class,args);
-	
-		
-		 
-	}
-	  /*@Bean
-	  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-	    return args -> {
-
-	      System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-	      String[] beanNames = ctx.getBeanDefinitionNames();
-	      Arrays.sort(beanNames);
-	      for (String beanName : beanNames) {
-	        System.out.println(beanName);
-	      }
-
-	    };
-	  }*/
-
-}
->>>>>>> 1b3cc95ac6ed742efad545ed04881aa5cb465025
